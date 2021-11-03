9ecf5568bbe158422bb41afb3f94db440e6ac964
wooks4098 <70507138+wooks4098@users.noreply.github.com>
2021-11-03 20:52:16 +0900

    오브젝트  UI

--- Assets/Wook/ObjectUI.cs
+++ Assets/Wook/ObjectUI.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class ObjectUI : MonoBehaviour
 {
-    public Transform Canvas;
+    public Camera camera;
+    public Transform obj;
+    public RectTransform image;
+    public RectTransform Canvas;
     public Transform player;
     private void Update()
     {
-        Vector3 l_vector = player.transform.position - transform.position;
-        l_vector.y = 0;
-        transform.rotation = Quaternion.LookRotation(l_vector).normalized;
+        Debug.Log(camera.WorldToScreenPoint(obj.position));
+
+        //image.transform.position = camera.WorldToScreenPoint(obj.position );
+        var screenPos = camera.WorldToScreenPoint(obj.position);
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(Canvas, screenPos, camera, out Vector2 pos);
+        image.localPosition = pos;
+        //Vector3 l_vector = player.transform.position - transform.position;
+        //l_vector.y = 0;
+        //transform.rotation = Quaternion.LookRotation(l_vector).normalized;
 
         //Canvas.LookAt(player);
-
+        //Mathf.Clamp(Hp - Damge, 0, max);
+        //Hp = Mathf.Max(Hp - Damge, 0);
     }
 }
