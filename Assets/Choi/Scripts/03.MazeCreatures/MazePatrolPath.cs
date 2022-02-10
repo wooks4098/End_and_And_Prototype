fc7af1b3e49551a6fd0fdf8dc1c82b7144d96431
po5-tato28 <65820373+po5-tato28@users.noreply.github.com>
2022-02-10 18:04:27 +0900

    Maze Creature Prototype 02 - 2
    - Creatures go around way points

--- Assets/Choi/Scripts/03.MazeCreatures/MazePatrolPath.cs
+++ Assets/Choi/Scripts/03.MazeCreatures/MazePatrolPath.cs
@@ -7,7 +7,15 @@ using UnityEngine;
 /// </summary>
 public class MazePatrolPath : MonoBehaviour
 {
+    // 자식의 개수
+    private int count;
+
+    // 지나온 길을 저장할 stack
     private Stack<Transform> wayPoints;
+
+    // 반대로 가는 중인지 (역순회 중인지) 체크하는 플래그
+    private bool isReverse; 
+
 
     private void Awake()
     {
@@ -15,16 +23,37 @@ public class MazePatrolPath : MonoBehaviour
     }
     private void Start()
     {
-        
+        count = transform.childCount;
+        isReverse = false;
     }
 
     public int GetNextIndex(int i)
     {
-        if (i + 1 == transform.childCount)
+        // stack이 전부 비었으면 순방향 이동
+        if (wayPoints.Count == 0) isReverse = false;
+        // stack이 자식 개수만큼 찼으면 역방향 이동
+        else if (wayPoints.Count >= (count - 1)) isReverse = true;
+
+        // 순방향이면
+        if (!isReverse)
         {
-            return 0;
+            Debug.Log("!isReverese");
+
+            // 값을 채운다
+            wayPoints.Push(transform.GetChild(i));
+            
+            return i + 1;
         }
-        return i + 1;
+        // 역방향이면
+        else 
+        {
+            Debug.Log("isReverese");
+
+            // 값을 1개씩 뺀다
+            wayPoints.Pop();
+
+            return i - 1;
+        }
     }
 
     public Vector3 GetWaypoint(int i)
